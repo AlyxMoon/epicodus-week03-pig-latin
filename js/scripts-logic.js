934a2d451fe1f792761cc09169d2facae7a3a459
AlyxMoon <allisterkmoon@gmail.com>
2021-06-09 10:12:00 -0700

    add logic to pass the first round of tests

--- js/scripts-logic.js
+++ js/scripts-logic.js
@@ -1,3 +1,11 @@
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
 function convertToPigLatin (text) {
+  let vowelIndex = vowels.indexOf(text[0])
+
+  if (vowelIndex > -1) {
+    text = `${text}way`
+  }
+
   return text
 }
